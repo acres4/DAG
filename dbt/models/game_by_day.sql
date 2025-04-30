3162496b4115ecb035d6014ee51c2f0fdd762aa9
JeffNaef <midimansland@gmail.com>
2025-04-30 16:46:26 -0700

    Update game_by_day.sql

--- dbt/models/game_by_day.sql
+++ dbt/models/game_by_day.sql
@@ -1,63 +1,55 @@
+{{ config(
+    materialized='table',
+    schema=env_var('SINGLESTORE_SCHEMA')
+) }}
 
 WITH source AS (
-
   SELECT
-    -- daily bucket
-    DATE(FROM_UNIXTIME(eventTime/1000))    AS day_bucket,
-
-    -- game identifier
-    gameId                                  AS game_id,
-
-    -- equipment/session serial
+    DATE(FROM_UNIXTIME(eventTime/1000)) AS day_bucket,
+    gameId                             AS game_id,
     ess,
-
-    -- basic wager/win metrics
-    SUM(updateCashableWager)                AS total_coin_in,
-    SUM(updateWon)                          AS total_coin_out,
-    AVG(updateCashableWager)                AS avg_bet_size,
+    SUM(updateCashableWager)           AS total_coin_in,
+    SUM(updateWon)                     AS total_coin_out,
+    AVG(updateCashableWager)           AS avg_bet_size,
     AVG(
-      CASE 
-        WHEN duration > 0 
-        THEN updateCashableWager / duration 
-        ELSE NULL 
+      CASE WHEN duration > 0
+           THEN updateCashableWager / duration
+           ELSE NULL
       END
-    )                                       AS avg_bet_velocity,
-
-    -- activity counts
-    COUNT(*)                                AS spin_count,
-    COUNT(DISTINCT sessionUuid)             AS session_count,
-
-    -- carded vs. uncarded sessions
-    COUNT(
-      DISTINCT CASE
-        WHEN playerCardNumber IS NOT NULL
-             AND playerCardNumber <> ''
-        THEN sessionUuid
-      END
-    )                                       AS carded_sessions,
-
-    COUNT(
-      DISTINCT CASE
-        WHEN playerCardNumber IS NULL
-             OR playerCardNumber = ''
-        THEN sessionUuid
-      END
-    )                                       AS uncarded_sessions,
-
-    -- unique players per game/day/ess
-    COUNT(
-      DISTINCT CASE
-        WHEN playerCardNumber IS NOT NULL
-             AND playerCardNumber <> ''
-        THEN playerCardNumber
-      END
-    )                                       AS unique_player_card_numbers
-
+    )                                  AS avg_bet_velocity,
+    COUNT(*)                           AS spin_count,
+    COUNT(DISTINCT sessionUuid)        AS session_count,
+    COUNT(DISTINCT CASE
+      WHEN playerCardNumber IS NOT NULL
+           AND playerCardNumber <> ''
+      THEN sessionUuid
+    END)                                AS carded_sessions,
+    COUNT(DISTINCT CASE
+      WHEN playerCardNumber IS NULL
+           OR playerCardNumber = ''
+      THEN sessionUuid
+    END)                                AS uncarded_sessions,
+    COUNT(DISTINCT CASE
+      WHEN playerCardNumber IS NOT NULL
+           AND playerCardNumber <> ''
+      THEN playerCardNumber
+    END)                                AS unique_player_card_numbers
   FROM {{ env_var('SINGLESTORE_DB') }}.session_data
-
-
   WHERE type = 'STUpdate'
-
   GROUP BY day_bucket, game_id, ess
-
 )
+
+SELECT
+  day_bucket,
+  game_id,
+  ess,
+  total_coin_in,
+  total_coin_out,
+  avg_bet_size,
+  avg_bet_velocity,
+  spin_count,
+  session_count,
+  carded_sessions,
+  uncarded_sessions,
+  unique_player_card_numbers
+FROM source;
