2c0a458fbd40db936b4224ac23f67c6a930652a7
JeffNaef <midimansland@gmail.com>
2025-04-30 14:20:29 -0700

    Update singlestore_dbt_rollups.py

--- dag/singlestore_dbt_rollups.py
+++ dag/singlestore_dbt_rollups.py
@@ -1,79 +1,82 @@
-"""
-DAG: singlestore_dbt_rollups
-Runs the 'game_by_day' dbt model for each SingleStore connection defined in Airflow, one task per connection.
-Compatible with Apache Airflow 2.10.5.
-"""
-
-from datetime import datetime
-import os
-import subprocess
-
 from airflow import DAG
-from airflow.hooks.base_hook import BaseHook
-from airflow.utils.db import create_session
-from airflow.models import Connection
-from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.decorators import task
+import subprocess
+import os
 
 # Default DAG arguments
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2025, 5, 1),
-    'depends_on_past': False,
+    "owner": "airflow",
+    "retries": 0,
 }
 
-# Function to run dbt for a single connection
-def run_dbt_for_conn(conn_id, **kwargs):
-    """
-    Fetch the Airflow Connection by ID and run the 'game_by_day' dbt model.
-    """
-    # Retrieve connection using BaseHook
-    conn = BaseHook.get_connection(conn_id)
-
-    # Build environment variables for dbt
-    env = os.environ.copy()
-    env.update({
-        'DBT_PROJECT_DIR': '/opt/airflow/dags/dbt',
-        'DBT_PROFILES_DIR': '/opt/airflow/dags/dbt',
-        'SINGLESTORE_HOST': conn.host or '',
-        'SINGLESTORE_PORT': str(conn.port or 3306),
-        'SINGLESTORE_USER': conn.login or '',
-        'SINGLESTORE_PASSWORD': conn.password or '',
-        'SINGLESTORE_SCHEMA': conn.schema or conn.extra_dejson.get('schema', ''),
-    })
-
-    # Construct dbt command
-    cmd = [
-        'dbt', 'run',
-        '--project-dir', env['DBT_PROJECT_DIR'],
-        '--profiles-dir', env['DBT_PROFILES_DIR'],
-        '--select', 'game_by_day'
-    ]
-
-    # Execute the dbt run
-    subprocess.run(cmd, check=True, env=env, cwd=env['DBT_PROJECT_DIR'])
-
-# Instantiate the DAG
-dag = DAG(
-    dag_id='singlestore_dbt_rollups',
+with DAG(
+    dag_id="singlestore_dbt_rollups",
     default_args=default_args,
-    schedule_interval='0 2 * * *',  # daily at 02:00 UTC
+    start_date=days_ago(1),
+    schedule_interval="0 2 * * *",  # daily at 02:00 UTC
     catchup=False,
-    tags=['dbt', 'singlestore', 'rollups'],
-)
+    tags=["dbt", "singlestore"],
+) as dag:
 
-# Fetch all SingleStore connections and create one task per connection
-with create_session() as session:
-    connections = (
-        session.query(Connection)
-               .filter(Connection.conn_id.like('singlestore\_%'))
-               .all()
-    )
+    @task
+    def get_singlestore_conns():
+        """
+        Query Airflow Connections for any conn_id starting with 'singlestore_'
+        and build a list of env-var dicts for each.
+        """
+        from airflow import settings
+        from airflow.models import Connection
 
-for conn in connections:
-    task_id = f"run_game_by_day_{conn.conn_id}"
-    PythonOperator(
-        task_id=task_id,
-        python_callable=run_dbt_for_conn,
-        op_kwargs={'conn_id': conn.conn_id},
-        dag=dag,
-    )
+        session = settings.Session()
+        conns = (
+            session.query(Connection)
+            .filter(Connection.conn_id.ilike("singlestore_%"))
+            .all()
+        )
+        session.close()
+
+        results = []
+        for conn in conns:
+            results.append({
+                "SINGLESTORE_HOST":     conn.host or "10.49.18.95",
+                "SINGLESTORE_PORT":     str(conn.port or 3306),
+                "SINGLESTORE_USER":     conn.login or "root",
+                "SINGLESTORE_PASSWORD": conn.password or "Acres1234",
+                "SINGLESTORE_DB":       conn.schema or "qa2_events",
+                "SINGLESTORE_SCHEMA": (
+                    conn.schema
+                    or conn.extra_dejson.get("schema", "")
+                    or ""
+                ),
+            })
+        return results
+
+    @task
+    def run_dbt(conn_env: dict):
+        """
+        Run `dbt run` under the given environment dict.
+        Assumes the Airflow worker image already has:
+          - dbt-core + dbt-singlestore installed
+          - your dbt project & profiles baked in under /opt/airflow/dags/dbt
+        """
+        # Merge SingleStore creds into the OS env
+        env = os.environ.copy()
+        env.update(conn_env)
+
+        # Execute dbt in the same container as the Airflow worker
+        subprocess.run(
+            [
+                "dbt", "run",
+                "--profiles-dir", "/opt/airflow/dags/dbt",
+                "--project-dir", "/opt/airflow/dags/dbt",
+                "--select", "game_by_day",
+            ],
+            check=True,
+            env=env,
+            cwd="/opt/airflow/dags/dbt",
+        )
+
+    # Wire up the dynamic mapping
+    conns = get_singlestore_conns()
+    run_dbt.expand(conn_env=conns)
