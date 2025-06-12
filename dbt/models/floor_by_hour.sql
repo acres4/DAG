7409628321d58fb518e15651bb81a616de24a7e1
funeconomist <gary.vartanian@gmail.com>
2025-06-12 16:47:59 -0700

    Update floor_by_hour.sql

--- dbt/models/floor_by_hour.sql
+++ dbt/models/floor_by_hour.sql
@@ -15,18 +15,30 @@ WITH source AS (
     -- group by floor location
     floorLocation                     AS floor_location,
 
-    -- metrics
-    AVG(updateCashableWager)          AS avg_bet_size,
-    COUNT(*)                          AS total_wagers,
-    SUM(updateCashableWager)          AS total_coin_in,
-    AVG(
-      CASE 
-        WHEN duration > 0 
-        THEN updateCashableWager / duration 
-        ELSE NULL 
-      END
-    )                                 AS avg_bet_velocity
+    -- lets try total, avg, cnt, max as counts, 
 
+    -- Total Metrics here
+    SUM(updateCashableWager + updateRestrictedWager 
+      + updateOORVCashableWager + updateOORVRestrictedWager)          AS total_coin_in,
+    SUM(updateCashableWager + updateOORVCashableWager)          AS total_cashable_coin_in,
+    SUM(updateWon + updateOORVWon) as total_coin_out,
+    SUM(updateJackpot + updateOORVJackpot) as total_jackpot,
+    total_coin_in-coin_out-total_jackpot as total_net_win,
+    max(maxCashableRisked) as max_player_budget, 
+
+    -- avg Metrics
+    AVG(updateCashableWager + updateRestrictedWager + updateOORVCashableWager + updateOORVRestrictedWager)          AS total_avg_bet_size,
+    -- TODO: avg Bet Velocity is gonna be a little bit higher hanging fruit. 
+    -- A little more difficult sql query. I will get back to it. Let me just get 
+
+    -- Counts
+    COUNT(*)                          AS total_spins,
+    count (distinct sessionUuid) as total_session_counts,
+    count (distinct assetNumber) as total_distinct_asset_counts,
+    count (distinct devInfoGameThemes) as total_distinct_game_themes
+    count (distinct playerCardNumber) as total_distinct_player_card_numbers
+    
+    
   FROM {{ env_var('SINGLESTORE_DB') }}.session_data
 
   WHERE type = 'STUpdate'
